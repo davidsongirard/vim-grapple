2fb6086adefa09f32ed114761a1a0b51bb97cddf
middleinitial <davidson@girardwebdesign>
2012-07-26 15:40:41 +0100

    update script now overwrites vimrc file

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# LINKS CONTENTS OF COMPONENTS TO VIM DIRECTORY FOR PROCESSING
+# Links contents of components to the .vim directory 
 cd ~
 rm -rf .vim
 ln -s ~/vim-grapple/components/ .vim
+
+# Overwrite the vimrc file to use our vimrc file instead
+echo "# Don't alter or vim-grapple will no longer function" > ~/.vimrc
+echo 'source ~/vim-grapple/configuration/vimrc.vim' >> ~/.vimrc
